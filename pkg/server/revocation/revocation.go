194b4fcee7b377402f3aa9a6ae2452eb1792fedc
Abdul Aris Rahmanudin <abdul.rahmanudin@accelbyte.net>
2023-05-24 04:29:32 +0000

    * feat: implement sample for revocation

---
+++ pkg/server/revocation/revocation.go
@@ -0,0 +1,98 @@
+// Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+package revocation
+
+import (
+	"fmt"
+
+	pb "lootbox-roll-function-grpc-plugin-server-go/pkg/pb"
+)
+
+type RevokeEntryType string
+
+const (
+	StatusFail    = "FAIL"
+	StatusSuccess = "SUCCESS"
+)
+
+const (
+	RevokeEntryTypeItem        RevokeEntryType = "ITEM"
+	RevokeEntryTypeCurrency    RevokeEntryType = "CURRENCY"
+	RevokeEntryTypeEntitlement RevokeEntryType = "ENTITLEMENT"
+)
+
+type Revocation interface {
+	Revoke(namespace string, userId string, quantity int32, request *pb.RevokeRequest) (*pb.RevokeResponse, error)
+}
+
+type ItemRevocation struct {
+}
+
+func (r *ItemRevocation) Revoke(namespace string, userId string, quantity int32, request *pb.RevokeRequest) (*pb.RevokeResponse, error) {
+	item := request.GetItem()
+	customRevocation := map[string]string{}
+	customRevocation["namespace"] = namespace
+	customRevocation["userId"] = userId
+	customRevocation["quantity"] = fmt.Sprintf("%d", quantity)
+	customRevocation["itemId"] = item.GetItemId()
+	customRevocation["sku"] = item.GetItemSku()
+	customRevocation["itemType"] = item.GetItemType()
+	customRevocation["useCount"] = fmt.Sprintf("%d", item.GetUseCount())
+	customRevocation["entitlementType"] = item.GetEntitlementType()
+	return &pb.RevokeResponse{
+		Status:           StatusSuccess,
+		CustomRevocation: customRevocation,
+	}, nil
+}
+
+type CurrencyRevocation struct {
+}
+
+func (r *CurrencyRevocation) Revoke(namespace string, userId string, quantity int32, request *pb.RevokeRequest) (*pb.RevokeResponse, error) {
+	currency := request.GetCurrency()
+	customRevocation := map[string]string{}
+	customRevocation["namespace"] = namespace
+	customRevocation["userId"] = userId
+	customRevocation["quantity"] = fmt.Sprintf("%d", quantity)
+	customRevocation["currencyNamespace"] = currency.GetNamespace()
+	customRevocation["currencyCode"] = currency.GetCurrencyCode()
+	customRevocation["balanceOrigin"] = currency.GetBalanceOrigin()
+
+	return &pb.RevokeResponse{
+		Status:           StatusSuccess,
+		CustomRevocation: customRevocation,
+	}, nil
+}
+
+type EntitlementRevocation struct {
+}
+
+func (r *EntitlementRevocation) Revoke(namespace string, userId string, quantity int32, request *pb.RevokeRequest) (*pb.RevokeResponse, error) {
+	entitlement := request.GetEntitlement()
+	customRevocation := map[string]string{}
+	customRevocation["namespace"] = namespace
+	customRevocation["userId"] = userId
+	customRevocation["quantity"] = fmt.Sprintf("%d", quantity)
+	customRevocation["entitlementId"] = entitlement.GetEntitlementId()
+	customRevocation["itemId"] = entitlement.GetItemId()
+	customRevocation["sku"] = entitlement.GetSku()
+	return &pb.RevokeResponse{
+		Status:           StatusSuccess,
+		CustomRevocation: customRevocation,
+	}, nil
+}
+
+var revocations = map[RevokeEntryType]Revocation{
+	RevokeEntryTypeItem:        &ItemRevocation{},
+	RevokeEntryTypeCurrency:    &CurrencyRevocation{},
+	RevokeEntryTypeEntitlement: &EntitlementRevocation{},
+}
+
+func GetRevocation(revocationType RevokeEntryType) (Revocation, error) {
+	if revocation, ok := revocations[revocationType]; ok {
+		return revocation, nil
+	}
+	return nil, fmt.Errorf("revocation type '%s' is not supported", revocationType)
+}
---
+++ pkg/server/revocationService.go
@@ -0,0 +1,41 @@
+// Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+package server
+
+import (
+	"context"
+
+	pb "lootbox-roll-function-grpc-plugin-server-go/pkg/pb"
+	"lootbox-roll-function-grpc-plugin-server-go/pkg/server/revocation"
+)
+
+type RevocationServiceServer struct {
+	pb.UnimplementedRevocationServer
+}
+
+func NewRevocationServiceServer() *RevocationServiceServer {
+	return &RevocationServiceServer{}
+}
+
+func (s *RevocationServiceServer) Revoke(ctx context.Context, req *pb.RevokeRequest) (*pb.RevokeResponse, error) {
+	revocationEntryType := revocation.RevokeEntryType(req.GetRevokeEntryType())
+	revocationObj, err := revocation.GetRevocation(revocationEntryType)
+	if err != nil {
+		return &pb.RevokeResponse{
+			Status: revocation.StatusFail,
+			Reason: err.Error(),
+		}, nil
+	}
+
+	revocationResp, err := revocationObj.Revoke(req.GetNamespace(), req.GetUserId(), req.GetQuantity(), req)
+	if err != nil {
+		return &pb.RevokeResponse{
+			Status: revocation.StatusFail,
+			Reason: err.Error(),
+		}, nil
+	}
+
+	return revocationResp, nil
+}
